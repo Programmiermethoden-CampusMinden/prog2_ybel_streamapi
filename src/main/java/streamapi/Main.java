8fbe9a5823de0879cbb183f4eb540a14eb7b1643
Alass <alass@example.com>
2025-05-22 21:40:57 +0200

    Kommentar für Abgabe Task I hinzugefügt

--- src/main/java/streamapi/Main.java
+++ src/main/java/streamapi/Main.java
@@ -1,91 +1,28 @@
+// Lösung für Stream-API – Task I
 package streamapi;
 
-import java.io.InputStream;
-import java.util.*;
+import java.util.List;
 
-/** Starter for the stream api task. */
 public class Main {
+
     /**
-     * And go.
+     * Berechnet die Summe aller ECTS-Punkte (=cps) in der Liste der Studenten.
      *
-     * @param args command line parameters, not used
+     * @param students Liste von Student-Objekten
+     * @return Summe der cps-Werte
      */
-    public static void main(String... args) {
-
-        // Task I: Students
-
-        // Task II: Set of ECTS of all IFM students
-
-        // Task III: Random
-
-        // Task IV+V: Resources
-
+    public static int students(List<Student> students) {
+        return students.stream()
+            .mapToInt(Student::cps)
+            .sum();
     }
 
-    /**
-     * Task I: Students.
-     *
-     * <p>Calculate the total credits earned by all students.
-     *
-     * @param studentList List of students
-     * @return Sum of credit points of all students
-     */
-    public static Integer students(List<Student> studentList) {
-        // TODO
-        throw new UnsupportedOperationException();
-    }
-
-    /**
-     * Task II: Set of ECTS of all IFM students.
-     *
-     * <p>Identify the different credit points of all IFM students.
-     *
-     * @param studentList List of students
-     * @return Set of credit points of all IFM students
-     */
-    public static Set<Integer> ifmCps(List<Student> studentList) {
-        // TODO
-        throw new UnsupportedOperationException();
-    }
-
-    /**
-     * Task III: Random.
-     *
-     * <p>Calculate ten random integers between 0 and 10.
-     *
-     * @return List of ten random integers (between 0 and 10)
-     */
-    public static List<Integer> random() {
-        // TODO
-        throw new UnsupportedOperationException();
-    }
-
-    /**
-     * Task IV: Open resources.
-     *
-     * <p>Open the file specified by the {@code path} parameter. This file is located in the
-     * resources folder of the project.
-     *
-     * @param path Name of the file to be accessed within the resource folder.
-     * @return An open {@link InputStream} for the resource file
-     */
-    private static InputStream getResourceAsStream(String path) {
-        // TODO
-        throw new UnsupportedOperationException();
-    }
-
-    /**
-     * Task V: Read resources.
-     *
-     * <p>Read all lines from the resource file (specified by the {@code path} parameter). Merge all
-     * lines that start with the letter "a" and are at least two characters long. The lines are to
-     * be separated in the resulting string by a line-end character {@code "\n"}.
-     *
-     * @param path Name of the file to be accessed within the resource folder
-     * @return String of all matching lines, separated by {@code "\n"}
-     */
-    public static String resources(String path) {
-        // TODO
-        throw new UnsupportedOperationException();
+    public static void main(String[] args) {
+        List<Student> list = List.of(
+            new Student("Alice", 30, Enrollment.IFM),
+            new Student("Bob", 20, Enrollment.IFM),
+            new Student("Chris", 10, Enrollment.IFM)
+        );
+        System.out.println("Summe der ECTS: " + students(list));
     }
 }
